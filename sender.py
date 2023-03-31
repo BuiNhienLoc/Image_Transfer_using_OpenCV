88592fb22e6ccc2984b2db757dc532a64a8b8b28
Loc Bui Nhien <15635@student.vgu.edu.vn>
2023-03-31 16:47:57 +0700

    init commit

---
+++ sender.py
@@ -0,0 +1,21 @@
+import socket
+import time
+import imagezmq
+import cv2
+ 
+sender = imagezmq.ImageSender(connect_to='tcp://laptop:5555')
+ 
+rpi_name = socket.gethostname() # send RPi hostname with each image
+cap = cv2.VideoCapture("traffic.mp4")
+
+while True:  # send images as stream until Ctrl-C
+    print("hello")
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    sender.send_image(rpi_name, frame)
+
+    if (cv2.waitKey(30)==27):
+            break
