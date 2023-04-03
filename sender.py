e878663d583a23b3badeac80ef3458f54c1612aa
Loc Bui Nhien <15635@student.vgu.edu.vn>
2023-04-03 09:21:49 +0700

    refind sender and receiver

--- sender.py
+++ sender.py
@@ -1,21 +1,24 @@
-import socket
-import time
-import imagezmq
 import cv2
- 
+import imagezmq
+
+# create a ZMQ sender object
 sender = imagezmq.ImageSender(connect_to='tcp://10.11.6.67:5555')
- 
-rpi_name = socket.gethostname() # send RPi hostname with each image
-cap = cv2.VideoCapture("traffic.mp4")
 
-while True:  # send images as stream until Ctrl-C
-    print("hello")
+# open the camera
+cap = cv2.VideoCapture(0)
+
+while True:
+    # read a frame from the camera
     ret, frame = cap.read()
-
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+    
+    # send the frame to the remote device
+    sender.send_image('jetson_nano', frame)
+    
+    # display the frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    sender.send_image(rpi_name, frame)
 
-    if (cv2.waitKey(30)==27):
-            break
+# release the camera and close the window
+cap.release()
+cv2.destroyAllWindows()
